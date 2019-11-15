c41842d7249b60db7710895ed8e70773e70c4b65
kevinjrodgers <kevjrodgers@gmail.com>
2019-11-14 23:15:53 -0800

    Add increase and decrease button functionality

--- popup.js
+++ popup.js
@@ -1,3 +1,93 @@
 let hours = 00;
 let minutes = 00;
 let seconds = 00;
+let hoursTimeLabel = document.getElementById("hoursTimer");
+let minutesTimeLabel = document.getElementById("minutesTimer");
+let secondsTimeLabel = document.getElementById("secondsTimer");
+// -------------------------------
+let increaseHoursBtn = document.getElementById("increaseHoursButton");
+let increaseMinutesBtn = document.getElementById("increaseMinutesButton");
+let increaseSecondsBtn = document.getElementById("increaseSecondsButton");
+increaseHoursBtn.addEventListener("click", () => {
+  increaseTime("hours");
+});
+increaseMinutesBtn.addEventListener("click", () => {
+  increaseTime("minutes");
+});
+increaseSecondsBtn.addEventListener("click", () => {
+  increaseTime("seconds");
+});
+// -------------------------------
+let decreaseHoursBtn = document.getElementById("decreaseHoursButton");
+let decreaseMinutesBtn = document.getElementById("decreaseMinutesButton");
+let decreaseSecondsBtn = document.getElementById("decreaseSecondsButton");
+decreaseHoursBtn.addEventListener("click", () => {
+  decreaseTime("hours");
+});
+decreaseMinutesBtn.addEventListener("click", () => {
+  decreaseTime("minutes");
+});
+decreaseSecondsBtn.addEventListener("click", () => {
+  decreaseTime("seconds");
+});
+
+function increaseTime(button) {
+  if(button == "hours") {
+    if(hours == 24) {
+      hours = 00;
+    }
+    else {
+      hours++;
+
+    }
+    hoursTimeLabel.innerHTML = hours;
+  }
+  else if(button == "minutes") {
+    if(minutes == 60) {
+      minutes = 00;
+    }
+    else {
+      minutes++;
+    }
+    minutesTimeLabel.innerHTML = minutes;
+  }
+  else if(button == "seconds") {
+    if(seconds == 60) {
+      seconds = 00;
+    }
+    else {
+      seconds++;
+    }
+    secondsTimeLabel.innerHTML = seconds;
+  }
+}
+
+function decreaseTime(button) {
+  if(button == "hours") {
+    if(hours == 0) {
+      hours = 24;
+    }
+    else {
+      hours--;
+    }
+    hoursTimeLabel.innerHTML = hours;
+  }
+  else if(button == "minutes") {
+    if(minutes == 0) {
+      minutes = 60;
+    }
+    else {
+      minutes--;
+    }
+    minutesTimeLabel.innerHTML = minutes;
+  }
+  else if(button == "seconds") {
+    if(seconds == 0) {
+      seconds = 60;
+    }
+    else {
+      seconds--;
+    }
+    secondsTimeLabel.innerHTML = seconds;
+  }
+}
